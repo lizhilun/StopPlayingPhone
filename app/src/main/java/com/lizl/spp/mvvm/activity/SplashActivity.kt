6757e6171422b8ad02d7c725c95645adebfc153e
李志伦 <313701855@qq.com>
2021-01-07 15:01:53 +0800

    添加启动界面

---
+++ app/src/main/java/com/lizl/spp/mvvm/activity/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.lizl.spp.mvvm.activity
+
+import com.lizl.spp.R
+import com.lizl.spp.databinding.ActivitySplashBinding
+import com.lizl.spp.mvvm.base.BaseActivity
+
+class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_splash)
+{
+    override fun initData()
+    {
+
+    }
+}
